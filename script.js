0edd16e90f4844e381900f246051aaf7bc471d4d
Jared Eklin <jaredeklin@gmail.com>
2017-12-09 16:54:57 -0700

    Add js variables

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+var websiteNameInput = document.querySelector('.website-name-input');
+var urlInput = document.querySelector('.website-url');
+var submitButton = document.querySelector('.submit-button');
+var websiteNameOutput = document.querySelector('h2')
+var urlOutput = document.querySelector('.website-link');
+var readButton = document.querySelector('#read-button');
+var deleteButton = document.querySelector('#delete-button');
+
