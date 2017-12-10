941ae32310df723efe0e3cf9c6604d1cad685fdb
Jared Eklin <jaredeklin@gmail.com>
2017-12-09 20:49:29 -0700

    Play with js objects

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ var urlOutput = document.querySelector('.website-link');
 var readButton = document.querySelector('#read-button');
 var deleteButton = document.querySelector('#delete-button');
 
+
+submitButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  websiteNameOutput.innerText = "hello";
+  console.log('click');
+});
+
+function Bookmark(websiteNameInput, urlInput) {
+  this.siteName = websiteNameInput;
+  this.url = urlInput;
+};
+
+var newBookmark = new Bookmark('google', 'www.google.com');
+
+
