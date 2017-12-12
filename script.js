3c3e160b3fb1c0754b3c0ed10861fe4690a399ee
Jared Eklin <jaredeklin@gmail.com>
2017-12-11 19:50:55 -0700

    Add conditional error alert

--- script.js
+++ script.js
@@ -4,26 +4,39 @@
 
 
 $('.submit-button').on('click', function () {
-  console.log("clicker");
-
-  var name = $('.website-name-input').val();
-  // $('h2').text(name);
-  var url = $('.website-url').val();
-  // $('.website-link').text(url);
-
-  var x = '<article class="bookmark-1"><h2>'+name+'</h2><hr><p class="website-link">'+url+'</p><hr><button class="link-button" id="read-button">Read</button><button class="link-button" id="delete-button">Delete</button></article>'
-  $('.section-2').prepend(x);
-  
-  getInfo();
+  // newBookmark();
+  errorCheck();
+  // changeFocus();
 });
 
 $('.section-2').on('click','#read-button', function () {
   console.log("read click");
   $(this).parent().toggleClass('read');
-})
+});
 
 $('.section-2').on('click', '#delete-button', function () {
   console.log('delete click');
   $(this).parent().remove()
-})
+});
 
+function newBookmark () {
+    var name = $('.website-name-input').val();
+  var url = $('.website-url').val();
+  var x = '<article class="bookmark-1"><h2>'+name+'</h2><hr><p class="website-link">'+url+'</p><hr><button class="link-button" id="read-button">Read</button><button class="link-button" id="delete-button">Delete</button></article>'
+  $('.section-2').prepend(x);
+}
+
+function errorCheck () {
+  if (($('.website-name-input').val() === '') || $('.website-url').val() === ''){
+    alert("Error!!!!");
+    // changeFocus();
+  } else {
+    newBookmark();
+  }
+};
+
+// function changeFocus () {
+//   $('.website-name-input').focus();
+//   $('.website-name-input').text('');
+
+// }
