43e44c14dffb3a5826cb992091cfb3a4c6125550
Jared Eklin <jaredeklin@gmail.com>
2017-12-12 12:27:45 -0700

    Add read and bookmark counter function

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 
 
-var websiteNameInput = document.querySelector('.website-name-input');
-var urlInput = document.querySelector('.website-url');
-var submitButton = document.querySelector('.submit-button');
-var websiteNameOutput = document.querySelector('h2')
-var urlOutput = document.querySelector('.website-link');
-var readButton = document.querySelector('#read-button');
-var deleteButton = document.querySelector('#delete-button');
-var bookmark = document.querySelector('.bookmark-1');
 
 
 $('.submit-button').on('click', function () {
@@ -15,19 +7,35 @@ $('.submit-button').on('click', function () {
   var name = $('.website-name-input').val();
   var url = $('.website-url').val();
   $('.section-2').prepend('<article class="bookmark-1"><h2>'+name+'</h2><hr><p class="website-link">'+url+'</p><hr><button class="link-button" id="read-button">Read</button><button class="link-button" id="delete-button">Delete</button></article>')
+  bookmarkCount();
 });
 
 $('.section-2').on('click','#read-button', function () {
   console.log("read click");
   $(this).parent().toggleClass('read');
+  readCount();
 });
 
 $('.section-2').on('click', '#delete-button', function () {
   console.log("delete click");
   $(this).parent().remove();
+  bookmarkCount();
 });
 
+$('.website-name-input').on('keyup', function () {
+  console.log('key up working??????');
+  $('.submit-button').prop('disabled', false);
+
+})
 
 
+function bookmarkCount () {
+  numberOfBookmarks = $('.section-2 .bookmark-1').length;
+  // $('.number-of-bookmarks').text(numberOfBookmarks);
+  console.log(numberOfBookmarks);
+}
 
-
+function readCount () {
+  var readBookmark = $('.section-2 .read').length;
+  console.log("read # " + readBookmark);
+}
