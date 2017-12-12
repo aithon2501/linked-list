b4a2f08b46b95bef1ad384491f8825e60f484045
mike duke <aithon@gmail.com>
2017-12-12 16:17:33 -0700

    Conflict Merge

--- script.js
+++ script.js
@@ -1,10 +1,54 @@
 
+$('.submit-button').on('click', function () {
+  errorCheck();
+  bookmarkCount();
+});
 
-var websiteNameInput = document.querySelector('.website-name-input');
-var urlInput = document.querySelector('.website-url');
-var submitButton = document.querySelector('.submit-button');
-var websiteNameOutput = document.querySelector('h2')
-var urlOutput = document.querySelector('.website-link');
-var readButton = document.querySelector('#read-button');
-var deleteButton = document.querySelector('#delete-button');
+$('.section-2').on('click','#read-button', function () {
+  $(this).parent().toggleClass('read');
+  readCount();
+});
+
+$('.section-2').on('click', '#delete-button', function () {
+  console.log("delete click");
+  $(this).parent().remove();
+  bookmarkCount();
+});
+
+$('.website-name-input, .website-url').on('keyup', function () {
+  enableButton();
+})
+
+function newBookmark () {
+  var name = $('.website-name-input').val();
+  var url = $('.website-url').val();
+  var newArticle = '<article class="bookmark-1"><h2>'+name+'</h2><hr><p class="website-link">'+url+'</p><hr><button class="link-button" id="read-button">Read</button><button class="link-button" id="delete-button">Delete</button></article>'
+  $('.section-2').prepend(newArticle);
+}
+
+function bookmarkCount () {
+  readCount();
+  var numberOfBookmarks = $('.section-2 .bookmark-1').length;
+  $('.number-of-bookmarks').text('Total bookmarks: ' +  numberOfBookmarks);
+}
+
+function readCount () {
+  var readBookmark = $('.section-2 .read').length;
+  $('.read-bookmarks').text('Read bookmarks: ' + readBookmark);
+}
+
+function enableButton() {
+  if (($('.website-name-input').val() === '') || $('.website-url').val() === ''){
+    $('.submit-button').prop('disabled', true);
+  } else {
+    $('.submit-button').prop('disabled', false);
+  }
+}
+function errorCheck () {
+  if (($('.website-name-input').val() === '') || $('.website-url').val() === ''){
+    alert("Error!!!!");
+  } else {
+    newBookmark();
+  }
+};
 
