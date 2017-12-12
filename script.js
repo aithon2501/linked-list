d3f26c39badee5f83a02a56276d2a36d6a773528
Jared Eklin <jaredeklin@gmail.com>
2017-12-11 19:29:26 -0700

    Add jquery

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 
 
-var websiteNameInput = document.querySelector('.website-name-input');
-var urlInput = document.querySelector('.website-url');
-var submitButton = document.querySelector('.submit-button');
-var websiteNameOutput = document.querySelector('h2')
-var urlOutput = document.querySelector('.website-link');
-var readButton = document.querySelector('#read-button');
-var deleteButton = document.querySelector('#delete-button');
+
+
+
+$('.submit-button').on('click', function () {
+  console.log("clicker");
+
+  var name = $('.website-name-input').val();
+  // $('h2').text(name);
+  var url = $('.website-url').val();
+  // $('.website-link').text(url);
+
+  var x = '<article class="bookmark-1"><h2>'+name+'</h2><hr><p class="website-link">'+url+'</p><hr><button class="link-button" id="read-button">Read</button><button class="link-button" id="delete-button">Delete</button></article>'
+  $('.section-2').prepend(x);
+  
+  getInfo();
+});
+
+$('.section-2').on('click','#read-button', function () {
+  console.log("read click");
+  $(this).parent().toggleClass('read');
+})
+
+$('.section-2').on('click', '#delete-button', function () {
+  console.log('delete click');
+  $(this).parent().remove()
+})
 
